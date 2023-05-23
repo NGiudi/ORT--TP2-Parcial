e6c340ec4f81bd825a2cdaf9974aa25130d9931f
Nicolás <nicolas.m.giudice@gmail.com>
2023-05-23 20:59:51 -0300

    agregue html para ingresar la nota

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 import express from "express";
 
+import { fileURLToPath } from "url";
+import path from "path";
+
 import { NotasRoute } from "./routes/NotasRoute.js";
 
 const PORT = 3005;
 
+/* fix para poder ver el html */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /* config */
 const app = express();
+
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/public/index.html");
+});
 
 app.use("/notas", new NotasRoute().start());
 
